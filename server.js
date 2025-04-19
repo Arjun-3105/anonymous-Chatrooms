4d7a814bbf2025938877df5b20d3ae2fc3656821
Arjun Chaudhary <arjun.chaudhary3105@gmail.com>
2025-04-19 18:34:12 +0530

    initial-commit

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Define pre-made rooms
+const premadeRooms = ["General", "Support", "Random", "Introductions"];
+
+// Serve static files correctly from /public
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+    console.log('a user connected');
+    // Send the list of rooms to the newly connected client
+    socket.emit('room list', premadeRooms);
+
+    socket.on('join room', (roomName) => {
+        if (premadeRooms.includes(roomName)) {
+            socket.join(roomName);
+            socket.room = roomName; // Store the room name on the socket
+
+            // Assign username *after* joining a room
+            const username = "Anonymous#" + Math.floor(1000 + Math.random() * 9000);
+            socket.username = username;
+
+            console.log(`${username} joined room: ${roomName}`);
+
+            // Notify others in the room
+            socket.to(roomName).emit("message", {
+                user: "System",
+                text: `${username} has joined the ${roomName} chat.`,
+            });
+            // Notify the user they joined
+             socket.emit("message", {
+                user: "System",
+                text: `You have joined the ${roomName} chat as ${username}.`,
+            });
+
+        } else {
+             socket.emit("message", {
+                user: "System",
+                text: `Error: Room "${roomName}" does not exist.`,
+            });
+        }
+    });
+
+
+    socket.on("chat message", (msg) => {
+        // Only broadcast to the sender's room
+        if (socket.room) {
+             io.to(socket.room).emit("message", { user: socket.username, text: msg });
+        } else {
+             // Handle case where user tries to send message before joining a room
+             socket.emit("message", {
+                user: "System",
+                text: `Please join a room first.`,
+            });
+        }
+    });
+
+    socket.on("disconnect", () => {
+        console.log('user disconnected');
+        // Only broadcast to the room the user was in
+        if (socket.room && socket.username) {
+             io.to(socket.room).emit("message", {
+                user: "System",
+                text: `${socket.username} has left the ${socket.room} chat.`,
+            });
+        }
+    });
+});
+
+server.listen(5000, () => {
+    console.log("Server running at http://localhost:5000");
+});
